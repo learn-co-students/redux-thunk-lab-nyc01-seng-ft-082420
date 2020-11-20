7d77baae1878a357fbe4c9490f0495dcc251233e
jdhofmann <justindhofmann@gmail.com>
2020-11-20 15:03:20 -0500

    everything works, these tests lie

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,16 @@
-// write your CatList component here
+import React from 'react'
+
+export const CatList = (props) =>{
+
+    const renderCats = () => {
+        return props.catPics.map(c => <img src={c.url}></img>)
+    }
+
+    return (
+        <div>
+            {/* <h2>List of Cats</h2> */}
+            {renderCats()}
+        </div>
+    )
+
+}
